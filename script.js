4c0906421a5839a4c3715ae229c3889c3da61d6d
pritiikhar <58639432+pritiikhar@users.noreply.github.com>
2020-07-30 03:28:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const submit = document.getElementById("submit");
+const error = document.getElementById("error");
+const phoneNumber = document.getElementById('phoneNumber');
+const name = document.getElementById('name');
+//------------------------------------------------------------Key validators----------------------------------------------------------------------
 const isNumericInput = (event) => {
     const key = event.keyCode;
     return ((key >= 48 && key <= 57) || // Allow number line
@@ -28,34 +33,29 @@ const isModifierKey = (event) => {
 };
 
 const formatMob = (event) => {
-    // Input must be of a valid number format or a modifier key, and not longer than ten digits
     if(!isNumericInput(event) && !isModifierKey(event)){
         event.preventDefault();
     }
 };
 
 const formatName = (event) => {
-    // Input must be of a valid number format or a modifier key, and not longer than ten digits
     if(!isAlphabetInput(event) && !isModifierKey(event)){
         event.preventDefault();
     }
 };
-
-//Mobile number validation
+//--------------------------------------------------------------------Mobile number validation----------------------------------------------------------------------------------
 const formatToPhone = (event) => {
+const target = event.target;
+mobValue = target.value;
+const input = target.value.replace(/\D/g,'').substring(0,10); // First ten digits of input only
+zip = input.substring(0,3);
+middle = input.substring(3,6);
+const last = input.substring(6,10);
+let erroR = document.getElementById("erroR");
+var logo = document.getElementById("comLogo");
 
 if(isModifierKey(event)) {return;}
-   
-    const target = event.target;
-    value = target.value;
-    const input = target.value.replace(/\D/g,'').substring(0,10); // First ten digits of input only
-    zip = input.substring(0,3);
-    middle = input.substring(3,6);
-    last = input.substring(6,10);
-    
-   
-
-    if(input.length >= 6){target.value = `(${zip})-${middle}-${last}`;
+if(input.length >= 6){target.value = `(${zip})-${middle}-${last}`;
     
 }
     else if(input.length >= 3){target.value = `(${zip}) - ${middle}`;
@@ -64,279 +64,76 @@ if(isModifierKey(event)) {return;}
     else if(input.length > 0){target.value = `(${zip}`;}
 
 mobOptr();
-stateName();
-    
-    if(value.length == 14){
- document.getElementById("erroR").innerHTML = "";
- document.getElementById("submit").disabled = false; 
-}
-else{
-document.getElementById("erroR").innerHTML = "Enter a valid 10 digit  mobile number";
-document.getElementById("submit").disabled = true; 
-}
-};
+displayStateName();
+   
+(mobValue.length == 14)?((erroR.innerHTML = ""), (submit.disabled = false)): ((erroR.innerHTML = "Enter a valid 10 digit  mobile number"),(submit.disabled = true));
 
-//Name Validation
+
+
+(620 <= zip && zip < 800)?((logo.style.display = "inline-block"),(logo.src = "jio.png"), (error.innerHTML = " ")):
+(800 <= zip && zip <= 919)?((logo.style.display = "inline-block"),(logo.src = "idea.png"), (error.innerHTML = " ")):
+(920 < zip && zip <= 1000)?((logo.style.display = "inline-block"),(logo.src = "vodafone.png"), (error.innerHTML = " ")):
+(mobValue.length == 14)?((error.innerHTML = " "),(submit.disabled = false)):
+((submit.disabled = true),(error.innerHTML = "*Enter a valid 10 digit  mobile number"),(logo.style.display = "none"));
+
+};
+//-----------------------------------------------------------Name Validation-----------------------------------------------------------------------------------------------------
 const formatToName = (event) => {
-    
-    const name = document.getElementById("name");
     const nameEx = /^[a-zA-Z ]*$/;
     const target =  event.target;
     const value = target.value;
     word = value.split(" ");
+    const nameError =  document.getElementById("NameError");
+    const name_Error =  document.getElementById("NameError");
     var char = " ";
     var i;
   
  for(i = 0; i < word.length; i++){
-char = word[i].length + "<br>";
-if(word[i].length<4){
-  document.getElementById("NameError").innerHTML = "*Each word of name should cointain min 4 characters ";
-  document.getElementById("submit").disabled = true;
-  }
-else if (word.length<2) {
-document.getElementById("NameError").innerHTML = "*Name should contain atleast two words";
-document.getElementById("submit").disabled = true;
-}
-else if (nameEx.test(name.value.toUpperCase()) == false ) {
- document.getElementById("Name_Error").innerHTML = "*Name should contain alphabets and spaces only";
- document.getElementById("submit").disabled = true;
-}  
-else{
-document.getElementById("NameError").innerHTML = "";
-document.getElementById("Name_Error").innerHTML = "";   
-document.getElementById("submit").disabled = false;
-}
-}    
+char = word[i].length;
+(word[i].length<4)?((nameError.innerHTML = "*Each word of name should cointain min 4 characters"),(submit.disabled = true)):
+(word.length<2)?((nameError.innerHTML = "*Name should contain atleast two words"),(submit.disabled = true)):
+(nameEx.test(name.value.toUpperCase()) == false )?((name_Error.innerHTML = "*Name should contain alphabets and spaces only"),(submit.disabled = true)):
+((nameError.innerHTML = ""),(submit.disabled = false),(name_Error.innerHTML = ""));
+} 
 };
+// ----------------------------------------------------Validates input fields on pressing or realising key------------------------------------------------------------
 
-
-
-// ----------------------------------------------------------------------------------------------------------------
-
-
-const phoneNumber = document.getElementById('phoneNumber');
 phoneNumber.addEventListener('keydown',formatMob);
 phoneNumber.addEventListener('keyup',formatToPhone);
 
-const name = document.getElementById('name');
 name.addEventListener('keydown',formatName);
 name.addEventListener('keyup',formatToName);
 
-//------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------Displays logo-----------------------------------------------------------------
 function mobOptr(){
-if(620 < zip && zip < 800){
- document.getElementById("comLogo").style.display = "inline-block";
- document.getElementById("comLogo").src = "jio.png";
- document.getElementById("error").innerHTML = " ";
-}
-else if(800 < zip && zip < 919){
- document.getElementById("comLogo").style.display = "inline-block";
- document.getElementById("comLogo").src = "idea.png";
- document.getElementById("error").innerHTML = " ";
-}
-else if(920 < zip && zip < 1000){
- document.getElementById("comLogo").style.display = "inline-block";
- document.getElementById("comLogo").src = "vodafone.png";
- document.getElementById("error").innerHTML = " ";
-}
-else {
-document.getElementById("comLogo").style.display = "none";
-document.getElementById("error").style.display = "block";
-document.getElementById("error").innerHTML = "*Invalid Mobile Number";
+var logo = document.getElementById("comLogo");
+
+(620 <= zip && zip < 800)?((logo.style.display = "inline-block"),(logo.src = "jio.png"), (error.innerHTML = " ")):
+(800 <= zip && zip <= 919)?((logo.style.display = "inline-block"),(logo.src = "idea.png"), (error.innerHTML = " ")):
+(920 < zip && zip <= 1000)?((logo.style.display = "inline-block"),(logo.src = "vodafone.png"), (error.innerHTML = " ")):
+((submit.disabled = true),(error.innerHTML = "*Invalid Mobile Number"),(logo.style.display = "none"));
 
 }
+
+//----------------------------------------------Displays state name------------------------------------------------------------------------------------------------------------------
+function displayStateName(){
+let statename = document.getElementById("stateName");
+let stateName = [];
+
+stateName[100] = 'Andhra Pradesh', stateName[125] = 'Arunachal Pradesh', stateName[150] = 'Assam', stateName[175] = 'Bihar', stateName[200] = 'Chhattisgarh',
+stateName[225] = 'Goa', stateName[250] = 'Gujarat', stateName[275] = 'Haryana', stateName[300] = 'Himachal Pradesh', stateName[325] = 'Jharkhand', stateName[350] = 'Karnataka', 
+stateName[375] = 'Kerala', stateName[400] = 'Madhya Pradesh', stateName[425] = 'Maharashtra', stateName[450] = 'Manipur', stateName[475] = 'Meghalaya', stateName[500] = 'Mizoram', 
+stateName[525] = 'Nagaland', stateName[550] = 'Odisha', stateName[575] = 'Punjab', stateName[600] = 'Rajasthan', stateName[625] = 'Sikkim', stateName[650] = 'Tamil Nadu', 
+stateName[675] = 'Telangana', stateName[700] = 'Tripura', stateName[725] = 'Uttar Pradesh', stateName[750] = 'Uttarakhand', stateName[775] = 'West Bengal', 
+stateName[800] = 'Andaman and Nicobar', stateName[825] = 'Chandigarh', stateName[850] = 'Daman and Diu & Dadar and Nagar Haveli', stateName[875] = 'Delhi', 
+stateName[900] = 'Jammu and Kashmir', stateName[925] = 'Ladakh', stateName[950] = 'Lakshadweep', stateName[975] = 'Puducherry';
+
+(stateName[middle] === undefined ) ? ((error.innerHTML = "*Invalid Mobile Number"),(submit.disabled = true), (statename.innerHTML = " ")):
+((statename.innerHTML = stateName[middle]),(submit.disabled = false));
+
 }
 
-//---------------------------------------------------------------------------------------------------------------------
-function stateName(){
- if(middle == 100){
- document.getElementById("p").innerHTML = "Andhra Pradesh";
- document.getElementById("error").style.display = "none";
- document.getElementById("submit").disabled = false;
-}
-else if(middle == 125){
-document.getElementById("p").innerHTML = "Arunachal Pradesh";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 150){
-document.getElementById("p").innerHTML = "Assam";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 175){
-document.getElementById("p").innerHTML = "Bihar";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 200){
-document.getElementById("p").innerHTML = "Chhattisgarh";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 225){
-document.getElementById("p").innerHTML = "Goa";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 250){
-document.getElementById("p").innerHTML = "Gujrat";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 275){
-document.getElementById("p").innerHTML = "Haryana";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 300){
-document.getElementById("p").innerHTML = "Himachal Pradesh";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 325){
-document.getElementById("p").innerHTML = "Jharkhand";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 350){
-document.getElementById("p").innerHTML = "Karnataka";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 375){
-document.getElementById("p").innerHTML = "Kerala";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 400){
-document.getElementById("p").innerHTML = "Madhya Pradesh";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 425){
-document.getElementById("p").innerHTML = "Maharashtra";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 450){
-document.getElementById("p").innerHTML = "Manipur";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 475){
-document.getElementById("p").innerHTML = "Meghalaya";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 500){
-document.getElementById("p").innerHTML = "Mizoram";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 525){
-document.getElementById("p").innerHTML = "Nagaland";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 550){
-document.getElementById("p").innerHTML = "Odisha";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 575){
-document.getElementById("p").innerHTML = "Punjab";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 600){
-document.getElementById("p").innerHTML = "Rajasthan";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 625){
-document.getElementById("p").innerHTML = "Sikkim";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 650){
-document.getElementById("p").innerHTML = "Tamil Nadu";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 675){
-document.getElementById("p").innerHTML = "Telangana";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 700){
-document.getElementById("p").innerHTML = "Tripura";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 725){
-document.getElementById("p").innerHTML = "Uttar Pradesh";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 750){
-document.getElementById("p").innerHTML = "Uttarakhand";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 775){
-document.getElementById("p").innerHTML = "West Bengal";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 800){
-document.getElementById("p").innerHTML = "Andaman and Nicobar Islands";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 825){
-document.getElementById("p").innerHTML = "Chandigarh";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 850){
-document.getElementById("p").innerHTML = "Dadra and Nagar Haveli and Daman and Diu";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 875){
-document.getElementById("p").innerHTML = "Delhi";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 900){
-document.getElementById("p").innerHTML = "Jammu and Kashmir";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 925){
-document.getElementById("p").innerHTML = "Ladakh";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 950){
-document.getElementById("p").innerHTML = "Lakshadweep";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else if(middle == 975){
-document.getElementById("p").innerHTML = "Puducherry";
-document.getElementById("error").style.display = "none";
-document.getElementById("submit").disabled = false;
-}
-else {
-document.getElementById("comLogo").style.display = "none";
-document.getElementById("error").innerHTML = "*Invalid Mobile Number";
-document.getElementById("p").innerHTML = "";
-document.getElementById("error").style.display = "block";
-document.getElementById("submit").disabled = true;
-}
-}
+//---------------------------------------------------------------------Loop to check if any input field is empty-------------------------------------------------------------------------------------------------
 
 setInterval(checkInput, 1);
 function checkInput()
@@ -344,26 +141,17 @@ function checkInput()
 var nameF = document.getElementById("name");
 var mobNum = document.getElementById("phoneNumber");
 var email = document.getElementById("email");
-if(nameF.value.length == 0)
-{
-document.getElementById("submit").disabled = true;
+let a;
+(nameF.value.length == 0)?(submit.disabled = true):(mobNum.value.length == 0)?(submit.disabled = true):(email.value.length == 0)?(submit.disabled = true):(a = 0);
 }
-else if(mobNum.value.length == 0){
-document.getElementById("submit").disabled = true;
-}
-else if(email.value.length == 0){
-document.getElementById("submit").disabled = true;
-}
-
-}
-
+//--------------------------------------------------------------------------Takes action after submit button is pressed----------------------------------------------------------------------------------------------------------------
 form.addEventListener('submit', (e) => {
 var form = document.getElementById("form");
 var otp = Math.floor(1000 + Math.random() * 9000);
 document.getElementById("otp1").innerHTML ="OTP is" + " " + otp;
-document.getElementById("submit").disabled = true;
+submit.disabled = true;
 localStorage.setItem("firstName", word[0]);
-localStorage.setItem("mobileNum", value);
+localStorage.setItem("mobileNum", mobValue);
 localStorage.setItem("otp", otp);
 url = "validate.html";
 window.open(url); 
